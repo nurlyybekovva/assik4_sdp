b71e6e0fbf6a73d57f0b105048a560e8431f40d9
Alua Nurlybekova <231481@astanait.edu.kz>
2024-10-19 23:38:52 +0500

    final

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,15 @@ public class Main {
         System.out.println("Surge fare(peak hours): $" + ride.calculateFare(4.5, 10, true));
 
         ride.setFareStrategy(new PremiumFareStrategy());
-        System.out.println("Premium fare: $" + ride.calculateFare(4.5, 10, false));
+        System.out.println("Premium fare: $" + ride.calculateFare(10, 20, false));
 
         ride.setFareStrategy(new DiscountFareStrategy());
-        System.out.println("Discount fare: $" + ride.calculateFare(4.5, 10, false));
+        System.out.println("Discount fare: $" + ride.calculateFare(10, 20, false));
+
+        try {
+            System.out.println("Invalid fare: " + ride.calculateFare(-10, 20, false));
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
