9e7a8ded739e1f28b0402e26a9da6afea0828754
Alua Nurlybekova <231481@astanait.edu.kz>
2024-10-23 14:16:15 +0500

    final

--- src/RegularFareStrategy.java
+++ src/RegularFareStrategy.java
@@ -1,6 +1,6 @@
 public class RegularFareStrategy implements FareStrategy{
-    private static final double rate_per_dist = 1.00;
-    private static final double rate_per_min = 0.25;
+    double rate_per_dist = 1.00;
+    double rate_per_min = 0.25;
     @Override
     public double calculateFare(double distance, double time) {
         return (rate_per_dist * distance + rate_per_min * time);
